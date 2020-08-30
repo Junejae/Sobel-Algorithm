55fb558eaddd9d41052274face768c8d0b5e036b
Junejae <55151385+Junejae@users.noreply.github.com>
2020-08-30 00:00:27 -0400

    Commit before submission

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 
 # How to run it
  gcc sobel.c
+
+ ./a.out <input.image> <output.image0> <output.image1> <output.image2>
+
+ example: ./a.out garb34.pgm mag.pgm low.pgm high.pgm
  
- ./a.out <input.image> <output.image1> <output.image2>
--- sobel.c
+++ sobel.c
@@ -1,10 +1,14 @@
+// Original code is from Dr. Niels Da Vitoria Lobo
 // Modified by Junejae Kim
-// UCF ID: 4826064
 // 8/29/2020
 
 #include <stdio.h>                          /* Sobel.c */
 #include <math.h>
 #include <stdlib.h> // added to use atoi()
+
+// Define high and low thresholds
+#define HI 110
+#define LO 40
 
         int pic[256][256];
         int outpicx[256][256];
@@ -19,8 +23,10 @@ char **argv;
 {
         int i,j,p,q,mr,sum1,sum2;
         double threshold;
-        FILE *fo1, *fo2, *fp1, *fopen();
+        FILE *fo0, *fo1, *fo2, *fp1, *fopen();
         char *foobar;
+
+        printf("Getting files...\n");
 
         argc--; argv++;
         foobar = *argv;
@@ -28,22 +34,38 @@ char **argv;
 
 	argc--; argv++;
 	foobar = *argv;
+	fo0=fopen(foobar,"wb");
+
+        argc--; argv++;
+	foobar = *argv;
 	fo1=fopen(foobar,"wb");
 
-        
+        argc--; argv++;
+	foobar = *argv;
+	fo2=fopen(foobar,"wb");
+
+        /* 
         argc--; argv++;
 	foobar = *argv;
 	threshold = atoi(foobar); // changed atof -> atoi
+         */
+
+        // Load the image
+        for (i=0;i<15;i++) // cut off the header
+          getc(fp1);
 
         for (i=0;i<256;i++)
         { for (j=0;j<256;j++)
                 {
                   pic[i][j]  =  getc (fp1);
-                  pic[i][j]  &= 0377;
+                  pic[i][j]  &= 0377; // clean the data with 1111 1111
                 }
         }
 
-        mr = 1;
+        printf("Calculation starts...\n");
+
+        // Scanning couvolution
+        mr = 1; // set the radious of mask array
         for (i=mr;i<256-mr;i++)
         { for (j=mr;j<256-mr;j++)
           {
@@ -62,6 +84,7 @@ char **argv;
           }
         }
 
+        // Store the magnitude of the gradient & find the highest value to scale
         maxival = 0;
         for (i=mr;i<256-mr;i++)
         { for (j=mr;j<256-mr;j++)
@@ -70,25 +93,76 @@ char **argv;
                                       (outpicy[i][j]*outpicy[i][j])));
              if (ival[i][j] > maxival)
                 maxival = ival[i][j];
-
-             // apply threshold   
-             if (ival[i][j] < threshold)
-                ival[i][j] = 0;
-
            }
         }
 
+        // Add header to see it on image viewer
+        fprintf(fo0, "P5\n");
+        fprintf(fo0, "%d %d\n", 256, 256);
+        fprintf(fo0, "255\n");
 
+        fprintf(fo1, "P5\n");
+        fprintf(fo1, "%d %d\n", 256, 256);
+        fprintf(fo1, "255\n");
+
+        fprintf(fo2, "P5\n");
+        fprintf(fo2, "%d %d\n", 256, 256);
+        fprintf(fo2, "255\n");
+
+        // Magnitude image
+        for (i=0;i<256;i++)
+          { for (j=0;j<256;j++)
+            {
+             ival[i][j] = (ival[i][j] / maxival) * 255;
+             fprintf(fo0,"%c",(char)((int)(ival[i][j])));
+            }
+          }
+
+        // Low threshold image
+        threshold = LO;
 
         for (i=0;i<256;i++)
           { for (j=0;j<256;j++)
             {
-             ival[i][j] = (ival[i][j] / maxival) * 255;            
-             fprintf(fo1,"%c",(char)((int)(ival[i][j])));
+             if (ival[i][j] > threshold)
+             {
+                fprintf(fo1,"%c",(char)255);
+             }
              
+             else
+             {
+                fprintf(fo1,"%c",(char)0);
+             }
             }
           }
         
+        // High threshold image
+        threshold = HI;
+
+        for (i=0;i<256;i++)
+          { for (j=0;j<256;j++)
+            {
+             if (ival[i][j] > threshold)
+             {
+                fprintf(fo2,"%c",(char)255);
+             }
+             
+             else
+             {
+                fprintf(fo2,"%c",(char)0);
+             }
+            }
+          }
+        
+
+        // Close files
+        fclose(fo0);
+        fclose(fo1);
+        fclose(fo2);
+        fclose(fp1);
+
+        printf("All tasks are done successfully!\n");
+
         return 0;
 
 }
